df00733662bd9d5d5913c979a28f0dc8d52783d0
robotsvision <robotsvision.vx@gmail.com>
2024-12-12 23:58:35 +0300

    [11:58]: code example. stack usage.

--- fast/stack/tests/stack_dump/main.c
+++ fast/stack/tests/stack_dump/main.c
@@ -1,5 +1,22 @@
+#include <stdlib.h>
 #include <stack.h>
 
+
 int main(void) {
-    return 0;
+
+    stack_conf_t conf;
+
+    conf.protection.hash = STACK_NO_HASH;
+    conf.protection.use_assert = true;
+    conf.protection.use_canary = true;
+    conf.protection.use_hardware_protect = true;
+    conf.size_of_element = sizeof(long double);
+
+    stack_t* my_stack = stack_create(conf, STACK_DEFAULT_ALLOCATION);
+
+    if (my_stack == NULL) {
+        return EXIT_FAILURE;
+    }
+
+    return EXIT_SUCCESS;
 }
