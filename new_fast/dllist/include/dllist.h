708dbeaeba55b44dfcdfc64b305aaa4aaa2d742e
robotsvision <robotsvision.vx@gmail.com>
2024-12-15 03:53:19 +0300

    [3:52]: list basic functions. create/delete. no docs and tests

---
+++ new_fast/dllist/include/dllist.h
@@ -0,0 +1,58 @@
+
+/**
+ * @file dllist.h
+ * @brief Doubly linked list implementation.
+ * 
+ * @version 1.0.0
+ * @date 15.12.24
+ * @author Matvey Rybalkin
+*/
+
+#ifndef DLLIST_H_
+#define DLLIST_H_
+
+/* ==================== Includes ==================== */
+
+#include <stddef.h>
+#include <stdint.h>
+#include <stdbool.h>
+
+/* ==================== Type definitions ==================== */
+
+typedef struct {
+    size_t size_of_element;
+    struct {
+        size_t num_of_elements;
+        size_t last_index;
+    } meta;
+
+    struct {
+        union {
+            uint8_t*     data_8bit;
+            uint16_t*    data_16bit;
+            uint32_t*    data_32bit;
+            uint64_t*    data_64bit;
+            double*      data_double;
+            long double* data_long_double;
+        } list_elements;
+        size_t* next_elements;
+        size_t* prev_elements;
+    } content;
+} dllist_t;
+
+extern dllist_t* dllist_create(size_t num_of_elements, size_t size_of_element);
+extern void dllist_destroy(dllist_t* list);
+extern bool dllist_is_empty(dllist_t* list);
+
+extern size_t dllist_get_size(dllist_t* list);
+extern size_t dllist_get_length(dllist_t* list);
+
+extern void dllist_push_front(dllist_t* list, void* new_element);
+extern void dllist_push_back(dllist_t* list, void* new_element);
+
+extern void dllist_pop_front(dllist_t* list, void* dest_element);
+extern void dllist_pop_back(dllist_t* list, void* dest_element);
+
+extern void dllist_get_element(dllist_t* list, size_t num_of_element, void* dest_element);
+
+#endif // DLLIST_H_
---
+++ new_fast/dllist/src/dllist.c
@@ -0,0 +1,136 @@
+
+/* ==================== Includes ==================== */
+
+#include <dllist.h>
+#include <stdlib.h>
+
+/* ==================== Public functions ==================== */
+
+dllist_t* dllist_create(size_t num_of_elements, size_t size_of_element) {
+
+    if ((num_of_elements | size_of_element) == NULL)
+        return NULL;
+
+    dllist_t* new_dllist = calloc(1, sizeof(dllist_t));
+
+    if (new_dllist == NULL)
+        return NULL;
+
+    void* elements_new_buffer = calloc(num_of_elements, size_of_element);
+
+    if (elements_new_buffer == NULL) {
+        free(new_dllist);
+        return NULL;
+    }
+
+    switch (size_of_element) {
+        case (1UL):
+            new_dllist->content.list_elements.data_8bit = (uint8_t*)elements_new_buffer;
+            break;
+
+        case (2UL):
+            new_dllist->content.list_elements.data_16bit = (uint16_t*)elements_new_buffer;
+            break;
+
+        case (4UL):
+            new_dllist->content.list_elements.data_32bit = (uint32_t*)elements_new_buffer;
+            break;
+
+        case (8UL):
+            new_dllist->content.list_elements.data_64bit = (uint64_t*)elements_new_buffer;
+            break;
+
+#   if (__SIZEOF_DOUBLE__ != 8UL)
+        case (sizeof(double)):
+            new_dllist->content.list_elements.data_double = (double*)elements_new_buffer;
+            break;
+#   endif
+
+        case (sizeof(long double)):
+            new_dllist->content.list_elements.data_long_double = (long double*)elements_new_buffer;
+            break;
+
+        default: {
+            free(new_dllist);
+            free(elements_new_buffer);
+            return NULL;
+        }
+    }
+}
+
+void dllist_destroy(dllist_t* list) {
+    switch (list->size_of_element) {
+        case (1UL):
+            free(list->content.list_elements.data_8bit);
+            break;
+
+        case (2UL):
+            free(list->content.list_elements.data_16bit);
+            break;
+
+        case (4UL):
+            free(list->content.list_elements.data_32bit);
+            break;
+
+        case (8UL):
+            free(list->content.list_elements.data_64bit);
+            break;
+
+#   if (__SIZEOF_DOUBLE__ != 8UL)
+        case (sizeof(double)):
+            free(list->content.list_elements.data_double);
+            break;
+#   endif
+
+        case (sizeof(long double)):
+            free(list->content.list_elements.data_long_double);
+            break;
+
+        default: {
+            __builtin_unreachable();
+        }
+    }
+    free(list);
+}
+
+bool dllist_is_empty(dllist_t* list) {
+    return (bool)(list->meta.last_index == 0);
+}
+
+
+size_t dllist_get_size(dllist_t* list) {
+    return list->size_of_element * (list->meta.last_index + 1);
+}
+
+size_t dllist_get_length(dllist_t* list) {
+    return list->meta.last_index;
+}
+
+void dllist_push_front(dllist_t* list, void* new_element) {
+
+    
+
+}
+
+void dllist_push_back(dllist_t* list, void* new_element) {
+
+
+}
+
+
+void dllist_pop_front(dllist_t* list, void* dest_element) {
+
+
+}
+
+void dllist_pop_back(dllist_t* list, void* dest_element) {
+
+
+}
+
+
+void dllist_get_element(dllist_t* list, size_t num_of_element, void* dest_element) {
+
+
+}
+
