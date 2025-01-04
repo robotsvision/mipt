dea22c72b671df0fd4f8a83cb8f382b534e9614d
robotsvision <robotsvision.vx@gmail.com>
2025-01-04 23:59:39 +0300

    [11:59]: stack protection general macros update

--- rebirth/stack/include/stack/protection.h
+++ rebirth/stack/include/stack/protection.h
@@ -9,28 +9,19 @@
 #include <utils/macro.h>
 
 #ifdef STACK_USE_CPP
-#define _SP_T(name) name
-#define _SP_L(name) name
-namespace stack {
-namespace protection {
-#else
-#define _SP_T(name) stack_protect_##name##_t
-#define _SP_L(name) STACK_PROTECT_##name
+namespace protect {
 #endif
 
 typedef struct {
-    uint8_t canary : 1;
-} PACKED_STRUCT(_SP_T(config));
+    uint8_t canary;
+} PACKED_STRUCT(_S_T(_protect_, config));
 
 typedef struct {
 
 } _SP_T(meta);
 
-
 #ifdef STACK_USE_CPP
-} // protection::
-} // stack::
-#undef _SP_T
-#undef _SP_L
+}
 #endif
+
 #endif // STACK_PROTECTION_H_
