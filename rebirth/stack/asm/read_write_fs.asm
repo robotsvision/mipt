a9d7544b551e2c235401625dfa6372bd00f04021
robotsvision <robotsvision.vx@gmail.com>
2025-01-05 15:47:07 -0800

    [11:54]: assembler fs minimal update

--- rebirth/stack/asm/read_write_fs.asm
+++ rebirth/stack/asm/read_write_fs.asm
@@ -17,34 +17,15 @@ section '.text' code readable executable
 
 ; Callable as Microsoft x64
 public read_fs
-public read_fs
+public write_fs
 
 ; -------------------------------------
 ;  size_t utf8_len(const utf8_string_t str)
 ; -------------------------------------
-utf8_len:
-    ; char* ptr = str;
-    mov rsi, rcx
-    ; size_t len = 0;
-    xor rdx, rdx
-next_char:
-    ; while ((*ptr) == '\0')
-    lodsb
-    test al, al
-    ; goto done
-    jz done
-
-    and al, 0C0h
-    cmp al, 80h
-    je next_char
-
-    ; len++;
-    inc rdx
-    jmp next_char
-done:
-    ; return len;
-    mov rax, rdx
+read_fs:
     ret
-    
+
+write_fs:
+    ret
 
 
